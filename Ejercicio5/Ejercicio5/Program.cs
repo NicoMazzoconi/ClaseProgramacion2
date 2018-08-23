ec5dcac0a50355f2c479720f1088985d71fee84b
NicoMazzoconi <36174156+NicoMazzoconi@users.noreply.github.com>
2018-08-22 22:15:37 -0300

    Add files via upload

---
+++ Ejercicio5/Ejercicio5/Program.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio5
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.Title = "Ejercicio Nro 5";
+      int numero;
+      string numeroStr;
+
+      numeroStr = Console.ReadLine();
+      if (int.TryParse(numeroStr, out numero))
+      {
+        
+      }
+    }
+  }
+}
