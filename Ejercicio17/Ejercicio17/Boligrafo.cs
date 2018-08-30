4a4e90a3f3cf5d0cc923fe3b901627ceb66a80bd
NicoMazzoconi <36174156+NicoMazzoconi@users.noreply.github.com>
2018-08-29 22:11:02 -0300

    Add files via upload

---
+++ Ejercicio17/Ejercicio17/Boligrafo.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boligrafos
+{
+    public class Boligrafo
+    {
+        private ConsoleColor color;
+        private short tinta;
+        const short cantidadTintaMaxima = 100;
+
+        public ConsoleColor GetColor()
+        {
+            return this.color;
+        }
+
+        public short GetTinta()
+        {
+            return this.tinta;
+        }
+
+        private void SetTinta(short tinta)
+        {
+            if (tinta < 0)
+            {
+                if (this.tinta - tinta >= 0)
+                    this.tinta = (short)(this.tinta - tinta);
+                else
+                    this.tinta = 0;
+            }
+            else if (tinta > 0)
+            {
+                if (this.tinta + tinta <= cantidadTintaMaxima)
+                    this.tinta = (short)(this.tinta + tinta);
+                else
+                    this.tinta = 100;
+            }
+        }
+
+        public void Recarga()
+        {
+            SetTinta(100);
+        }
+
+        public void Pintar(int gasto, out string dibujo)
+        {
+            short i;
+            dibujo = "";
+            if (gasto >= this.tinta)
+            {
+                for (i = 0; i < tinta; i++)
+                    dibujo = dibujo + "*";
+                SetTinta(-100);
+            }
+            else
+                for (i = 0; i < gasto; i++)
+                    dibujo = dibujo + "*";
+            SetTinta((short)-i);
+        }
+
+        public Boligrafo BoligrafoCreate(short tinta, ConsoleColor color)
+        {
+            Boligrafo boligrafo = new Boligrafo();
+            if (tinta > 100)
+                boligrafo.tinta = 100;
+            else if (tinta < 0)
+                boligrafo.tinta = 0;
+            else
+                boligrafo.tinta = tinta;
+            boligrafo.color = color;
+            return boligrafo;
+        }
+    }
+}
---
+++ Ejercicio17/Ejercicio17/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Boligrafos;
+
+namespace Ejercicio17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio NRO 17";
+            Boligrafo azul = new Boligrafo(); 
+            azul.BoligrafoCreate(100, ConsoleColor.Blue);
+          
+        }
+    }
+}
