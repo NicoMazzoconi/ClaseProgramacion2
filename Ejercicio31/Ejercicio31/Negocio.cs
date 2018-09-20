eace9f5a7e901b80cfa6cc173568dd124dc52ded
alumno <alumno>
2018-09-19 22:06:34 -0300

    Empezando el 36

--- Ejercicio31/Ejercicio31/Negocio.cs
+++ Ejercicio31/Ejercicio31/Negocio.cs
@@ -8,5 +8,65 @@ namespace Ejercicio31
 {
     class Negocio
     {
+        private PuestoAtencion caja;
+        private Queue<Cliente> clientes;
+        private string nombre;
+
+        private Negocio()
+        {
+            this.caja = new PuestoAtencion(Puesto.Caja1);
+        }
+
+        public Negocio(string nombre) : this()
+        {
+            this.nombre = nombre;
+        }
+
+        public Cliente Cliente
+        {
+            get
+            {
+                return clientes.Dequeue();
+            }
+
+            set
+            {
+                bool agregar = this + value;
+            }
+        }
+        public static bool operator ==(Negocio n, Cliente c)
+        {
+            foreach(Cliente s in n.clientes)
+            {
+                if (s == c)
+                    return true;
+            }
+            return false;
+        }
+
+        public static bool operator !=(Negocio n, Cliente c)
+        {
+            return !(n == c);
+        }
+
+        public static bool operator +(Negocio n, Cliente c)
+        {
+            if(n != c)
+            {
+                n.clientes.Enqueue(c);
+                return true;
+            }
+            return false;
+        }
+
+        public static bool operator ~(Negocio n)
+        {
+            if (n.caja.Atender(n.Cliente))
+            {
+                return true;
+            }
+            return false;
+        }
     }
+
 }
--- Ejercicio31/Ejercicio31/PuestoAtencion.cs
+++ Ejercicio31/Ejercicio31/PuestoAtencion.cs
@@ -3,10 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace Ejercicio31
 {
+    enum Puesto
+    {
+        Caja1,
+        Caja2
+    }
+
     class PuestoAtencion
     {
+        private Puesto puesto;
+        private static int numeroActual;
+
+        static PuestoAtencion()
+        {
+            numeroActual = 0;
+        }
+        
+        public PuestoAtencion(Puesto puesto)
+        {
+            this.puesto = puesto;
+        }
+
+        public bool Atender(Cliente cli)
+        {
+            Thread.Sleep(500);
+            return true;
+        }
+
+        static int NumeroActual
+        {
+            get
+            {
+                numeroActual++;
+                return numeroActual;
+            }
+        }
     }
 }
