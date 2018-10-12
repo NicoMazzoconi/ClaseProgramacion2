549a7f32ac600e04afd33b2d4b9a7e463ec41772
alumno <alumno>
2018-10-11 22:13:00 -0300

    Ejercicio 44

---
+++ Ejercicio44/TestUnitario/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ejercicio44;
+
+namespace TestUnitario
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Centralita testCent = new Centralita("TestUnitario");
+            Assert.IsNotNull(testCent.Llamadas);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Centralita testCent = new Centralita("TestUnitario");
+            Local llamada = new Local("Pehuajo", 12, "Capital", 25);
+            Local llamada2 = new Local("Pehuajo", 2, "Capital", 45);
+
+            try
+            {
+                testCent += llamada;
+                testCent += llamada2;
+            }
+            catch(Exception ex)
+            {
+                Assert.IsInstanceOfType(ex, );
+            }
+
+            
+        }
+    }
+}
