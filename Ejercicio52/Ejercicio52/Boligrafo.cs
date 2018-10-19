4a8d0e1d31656db41ec45d909455534ce212619c
alumno <alumno>
2018-10-18 22:09:40 -0300

    Interfaces

---
+++ Ejercicio52/Ejercicio52/Boligrafo.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio52
+{
+    public class Boligrafo : IAcciones
+    {
+
+        private ConsoleColor colorTinta;
+        private float tinta;
+
+        public Boligrafo(int unidades, ConsoleColor color)
+        {
+            ((IAcciones)this).UnidadesDeEscritura = unidades;
+            ((IAcciones)this).Color = color;
+        }
+
+        public ConsoleColor Color
+        {
+            get
+            {
+                return colorTinta;
+            }
+            set
+            {
+                colorTinta = value;
+            }
+        }
+
+        public float UnidadesDeEscritura
+        {
+            get
+            {
+                return tinta;
+            }
+            set
+            {
+                if (tinta != 0)
+                    tinta = tinta + value;
+                else
+                    tinta = value;
+            }
+        }
+
+        public EscrituraWrapper Escribir(string texto)
+        {
+            StringBuilder escrito = new StringBuilder();
+            float tamaño = (float)texto.Length;
+            int i = 0;
+            while (tamaño >= 0.3)
+            {
+                escrito.AppendFormat("{0}", texto[i]);
+                i++;
+                tamaño = (float)(tamaño - 0.3);
+            }
+            return new EscrituraWrapper(escrito.ToString(), ((IAcciones)this).Color);
+        }
+
+        public bool Recargar(int unidades)
+        {
+            UnidadesDeEscritura = unidades;
+            return true;
+        }
+    }
+}
---
+++ Ejercicio52/Ejercicio52/Lapiz.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio52
+{
+    public class Lapiz  : IAcciones
+    {
+        private float tamañoMina;
+
+        public Lapiz(int mina)
+        {
+            tamañoMina = mina;
+        }
+        ConsoleColor IAcciones.Color
+        {
+            get
+            {
+                return ConsoleColor.Gray;
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        float IAcciones.UnidadesDeEscritura
+        {
+            get
+            {
+                return tamañoMina;
+            }
+            set
+            {
+                tamañoMina = value;
+            }
+        }
+
+        EscrituraWrapper IAcciones.Escribir(string texto)
+        {
+            StringBuilder escrito = new StringBuilder();
+            float tamaño = (float)texto.Length;
+            int i = 0;
+            while(tamaño >= 0.1)
+            {
+                escrito.AppendFormat("{0}", texto[i]);
+                i++;
+                tamaño = (float)(tamaño - 0.1);
+            }
+            return new EscrituraWrapper(escrito.ToString(), ((IAcciones)this).Color);
+        }
+
+        bool IAcciones.Recargar(int unidades)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return "Es un Lapiz, de color " + ((IAcciones)this).Color.ToString() + " y tiene un nivel de tinta de " + ((IAcciones)this).UnidadesDeEscritura;
+        }
+    }
+}
