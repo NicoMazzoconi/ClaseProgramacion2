c2efba2f05d7afb13dc440c77dd97d0b928a4600
alumno <alumno>
2018-11-01 22:07:37 -0300

    sql

---
+++ Ejercicio60/Ejercicio60/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Ejercicio60
+{
+    public partial class Form1 : Form
+    {
+        private SqlConnection conexion;
+
+        public Form1()
+        {
+            InitializeComponent();
+            string conexionStr = "Data Source=[LAB3PC27\\SQLEXPRESS];Initial Catalog=[AdventureWorks2012];Integrated Security=True";
+            conexion = new SqlConnection(conexionStr);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Ejercicio60/Ejercicio60/SqlConexion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Ejercicio60
+{
+    class SqlConexion
+    {
+        private SqlConnection conexion;
+        public SqlConexion(string source, string nombre)
+        {
+            string conexionStr = "Data Source=[" + source + "];Initial Catalog=[" + nombre + "];Integrated Security=True";
+            conexion = new SqlConnection(conexionStr);
+        }
+    }
+}
